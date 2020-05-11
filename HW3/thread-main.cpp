3cf02f358cfdc8c6683efa0f18c399401d68045d
Yun <enya.ohfee.0901@gmail.com>
2020-05-11 22:13:01 +0800

    HW2
    make it compilable in c89
    --------
    remove exe files

--- HW3/thread-main.cpp
+++ HW3/thread-main.cpp
@@ -1,82 +1,3 @@
-<<<<<<< HEAD
-#include <string.h>
-
-#include <cmath>
-#include <iostream>
-#include <vector>
-
-#include "thread.h"
-
-using namespace std;
-
-int main() {
-    int n, tem, k;
-    char buf[200];  //for standard output
-    vector<int> x;
-    vector<vector<int>> B;
-    //PrefixSumThread *prefixSumThread[8];
-    vector<PrefixSumThread*> prefixSumThread;
-
-    sprintf(buf, "Concurrent Prefix Sum Computation\n");
-    write(1, buf, strlen(buf));
-
-    //Read input array
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> tem;
-        x.push_back(tem);
-    }
-    k = log2(n);  //How many runs
-    //Prepare B[*,*] of k+1 rows and n columns.
-    for (int i = 0; i < k + 1; i++) {
-        B.push_back(x);
-    }
-    //Prepare n PrefixSumThreads
-    for (int i = 0; i < n; i++) {
-        prefixSumThread.push_back(new PrefixSumThread(&B, 0, 0));
-    }
-
-    sprintf(buf, "Number of input data = %d\n", n);
-    write(1, buf, strlen(buf));
-    sprintf(buf, "Input array:\n");
-    for (int i = 0; i < n; i++) {
-        sprintf(buf + strlen(buf), "%4d", x[i]);
-    }
-    sprintf(buf + strlen(buf), "\n");
-    write(1, buf, strlen(buf));
-
-    //start the prefixSum thread
-    for (int run = 1; run < k + 1; run++) {
-        sprintf(buf, "Run %d:\n", run);
-        write(1, buf, strlen(buf));
-        for (int index = 0; index < n; index++) {
-            prefixSumThread[index] = new PrefixSumThread(&B, run, index);
-            prefixSumThread[index]->Begin();
-        }
-        sprintf(buf, "Result after run %d:\n", run);
-        for (int i = 0; i < n; i++) {
-            sprintf(buf + strlen(buf), "%4d", B[run][i]);
-        }
-        sprintf(buf + strlen(buf), "\n");
-        write(1, buf, strlen(buf));
-    }
-
-    //wait for the prefixSum threads to finish
-    for (int index = 0; index < n; index++) {
-        prefixSumThread[index]->Join();
-    }
-
-    sprintf(buf, "Final result after run %d:\n", k);
-    for (int i = 0; i < n; i++) {
-        sprintf(buf + strlen(buf), "%4d", B[k][i]);
-    }
-    sprintf(buf + strlen(buf), "\n");
-    write(1, buf, strlen(buf));
-
-    Exit();
-
-    return 0;
-=======
 // ----------------------------------------------------------- 
 // NAME : YunTsen Lo                         User ID: 108598056 
 // DUE DATE : 05/04/2020                                       
@@ -167,5 +88,4 @@ int main() {
     Exit();
 
     return 0;
->>>>>>> 562ad7b9ef3d2d6290073f965e5e23f048514ab6
 }
--- HW3/thread.cpp
+++ HW3/thread.cpp
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-// --------------------------------------------------------------//
-// File : quicksort.cpp                                          //
-// PROGRAM DISCRIPTION                                           //
-//    Class implemention file for the QuickSortThread class      //
-// ------------------------------------------------------------- //
-
-#include "thread.h"
-
-#include <string.h>
-
-#include <cmath>
-#include <iostream>
-
-using namespace std;
-
-//---------------------------------------------------------------//
-//  QuickSort Class:
-//     constructor
-//---------------------------------------------------------------//
-
-PrefixSumThread::PrefixSumThread(vector<vector<int>>* B, int stage, int index) : _B(B), _stage(stage), _index(index) {
-    ThreadName.seekp(0, ios::beg);
-    ThreadName << "Stage:"
-               << _stage << ', Index:' << _index
-               << '\0';
-}
-
-//---------------------------------------------------------------//
-//  QuickSort Thread Class body:
-//     each thread sort half of the a
-//---------------------------------------------------------------//
-
-void PrefixSumThread::ThreadFunc() {
-    Thread::ThreadFunc();
-
-    int n = (*_B)[0].size();
-    int k = log(n);
-    int gap = pow(2, _stage - 1);
-    char buf[200];  //for standard output
-
-    sprintf(buf, "     Thread %d Created\n", _index);
-    write(1, buf, strlen(buf));
-
-    if (_index < gap) {  //B[0..gap-1] that need no calculation
-        sprintf(buf, "     Thread %d copies x[%d]\n", _index, _index);
-        write(1, buf, strlen(buf));
-        (*_B)[_stage][_index] = (*_B)[_stage - 1][_index];
-    } else {  //B[gap..n] that need calculation
-        sprintf(buf, "     Thread %d computes x[%d] + x[%d]\n", _index, _index, _index - gap);
-        write(1, buf, strlen(buf));
-        (*_B)[_stage][_index] = (*_B)[_stage - 1][_index] + (*_B)[_stage - 1][_index - gap];
-    }
-
-    sprintf(buf, "     Thread %d exits\n", _index);
-    write(1, buf, strlen(buf));
-    Exit();
-}
-
-// end of QuickSort.cpp
-=======
 // ----------------------------------------------------------- 
 // NAME : YunTsen Lo                         User ID: 108598056 
 // DUE DATE : 05/04/2020                                       
@@ -127,4 +66,3 @@ void PrefixSumThread::ThreadFunc() {
 }
 
 // end of thread.cpp
->>>>>>> 562ad7b9ef3d2d6290073f965e5e23f048514ab6
